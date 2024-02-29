6c5b6960a838819dada9c982e89c72b11d4e656e
umeshsurange@gmail.com <138711278+umeshsurnage@users.noreply.github.com>
2024-02-29 16:34:05 +0530

    login start

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes, Navigate} from 'react-router-dom'
+import Login from './Login/Login'
+import {Dashboard} from './Pages/Dashboard'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<>
+    
+    <Routes>
+      <Route path="/" element={<Navigate to='/Login' />}></Route>
+      <Route path="/Login" element={<Login></Login>}></Route>
+      {/* <Route path='/Dashboard' element={<Dashboard></Dashboard>}></Route> */}
+    </Routes>
+    </>
   );
 }
 
